d337e3ba2df0bec4792ecf1032c9f9ca1579e3af
Daniel Jing <jpdan727@gmail.com>
2023-07-13 16:13:55 +0800

    Add files via upload

---
+++ toolbox/largest_number.py
@@ -0,0 +1,19 @@
+
+from itertools import permutations
+
+def largest_number_naive(numbers):
+    numbers = list(map(str, numbers))
+
+    largest = 0
+
+    for permutation in permutations(numbers):
+        largest = max(largest, int("".join(permutation)))
+
+    largest = max(int("".join(permutation)) for permutation in permutations(numbers))
+    return largest
+
+
+if __name__ == '__main__':
+    _ = int(input())
+    input_numbers = input().split()
+    print(largest_number_naive(input_numbers))
